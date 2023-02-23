740977c54b0d6839980d3aba782ead1bc5b9a3cd
Ruslan Ergashev <rergashev@hotmail.com>
2023-02-23 17:22:47 +0500

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import news, CurrentPost,  \
-     subscriptions
+from .views import news, CurrentPost,  
 
 urlpatterns = [
     path('news/', NewsList.as_view()),
        path('post/<int:pk>', CurrentPost.as_view()),
-    path('post/subscriptions/', subscriptions, name='subscriptions'),
+    
 ]
