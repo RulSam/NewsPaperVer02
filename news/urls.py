c14d93adefd5d2efe3d041a5f946813cbf40baff
Ruslan Ergashev <rergashev@hotmail.com>
2023-02-23 17:18:53 +0500

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -4,18 +4,6 @@ from .views import NewsList, CurrentNews, ArticlesList, CurrentArticles, news, C
 
 urlpatterns = [
     path('news/', NewsList.as_view()),
-    path('news/<int:pk>', CurrentNews.as_view()),
-    path('articles/', ArticlesList.as_view()),
-    path('articles/<int:pk>', CurrentArticles.as_view()),
-    path('post/', news.as_view()),
-    path('', news.as_view()),
-    path('post/<int:pk>', CurrentPost.as_view()),
-    path('search/', search.as_view()),
-    path('news/create/', NewsCreate.as_view()),
-    path('news/<int:pk>/edit/', NewsEdit.as_view()),
-    path('news/<int:pk>/delete/', NewsDelete.as_view()),
-    path('articles/create/', ArticlesCreate.as_view()),
-    path('articles/<int:pk>/edit/', ArticlesEdit.as_view()),
-    path('articles/<int:pk>/delete/', ArticlesDelete.as_view()),
+       path('post/<int:pk>', CurrentPost.as_view()),
     path('post/subscriptions/', subscriptions, name='subscriptions'),
 ]
